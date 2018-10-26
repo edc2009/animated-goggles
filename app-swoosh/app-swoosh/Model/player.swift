03d0ecb2b1eee85ffd9645725e1d24a463b3b013
Christopher Gibson <chris.edc.2010@gmail.com>
2018-10-25 17:21:20 -0700

    added button functionality

---
+++ app-swoosh/app-swoosh/Model/player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  app-swoosh
+//
+//  Created by Christopher on 10/25/18.
+//  Copyright © 2018 Christopher. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
